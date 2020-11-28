00e6c9c00a772ab65b061caa08bf0f34e47e3a30
Paulo Araújo <paulojosejr95@gmail.com>
2020-11-28 23:09:00 +0000

    useEffect and useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = 'c878ee2a'
+  const APP_KEY = 'f074d37b821098fe4dedf958b02aa54c'
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+    console.log('Effect has been run')
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">Search</button>
+      </form>
+      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
     </div>
-  );
+  )
 }
 
 export default App;
