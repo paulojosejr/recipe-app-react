dbea2e99a86c48bb4b7d8fff7e906753e2bd2f60
Paulo Araújo <paulojosejr95@gmail.com>
2020-11-30 21:36:20 +0000

    add ingredients and styles

--- src/App.js
+++ src/App.js
@@ -30,17 +30,24 @@ const App = () => {
   const getSearch = e => {
     e.preventDefault()
     setQuery(search)
+    setSearch('')
   }
-
   return (
     <div className="App">
       <form onSubmit={getSearch} className="search-form">
         <input className="search-bar" type="text" value={search} onChange={updateSearch} />
         <button className="search-button" type="submit">Search</button>
       </form>
+      <div className="recipes">
       {recipes.map(recipe => (
-        <Recipe key={recipe.recipe.label} title={recipe.recipe.label} calories={recipe.recipe.calories} image={recipe.recipe.image}/>
+        <Recipe 
+        key={recipe.recipe.calories}
+        title={recipe.recipe.label}
+        calories={recipe.recipe.calories}
+        image={recipe.recipe.image}
+        ingredients={recipe.recipe.ingredients}/>
       ))}
+      </div>
     </div>
   )
 }
