7c0a3e40fce23bce622dfa77044a9e99d23f9b1f
Paulo Araújo <paulojosejr95@gmail.com>
2021-07-23 11:52:24 +0100

    update search bar

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const App = () => {
   return (
     <div className="App">
       <form onSubmit={getSearch} className="search-form">
-        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+        <input placeholder="Search your recipe" className="search-bar" type="text" value={search} onChange={updateSearch} />
         <button className="search-button" type="submit">Search</button>
       </form>
       <div className="recipes">
