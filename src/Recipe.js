da6b9133f4a38df176658ec5c2344d11a5a1462d
Paulo Araújo <paulojosejr95@gmail.com>
2021-07-28 21:07:00 +0100

    update recipe card size

--- src/Recipe.js
+++ src/Recipe.js
@@ -4,10 +4,10 @@ import style from './recipe.module.css'
 const Recipe = ({title, calories, image, ingredients}) => {
     return(
         <div className={style.recipe}>
-            <h1>{title}</h1>
+            <h1 className={style.recipeTitle}>{title}</h1>
             <ol>
                 {ingredients.map(ingredient => (
-                    <li>{ingredient.text}</li>
+                    <li className={style.recipeIngredient}>{ingredient.text}</li>
                 ))}
             </ol>
                 <p>Calories: {calories.toFixed(0)}</p>
