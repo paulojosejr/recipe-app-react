c0ba77f3b534d9c4a772a968ee61c9192f04fd63
Paulo Araújo <paulojosejr95@gmail.com>
2021-08-04 12:09:47 +0100

    add homepage component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Home.css'
+import background from "./backgroundImage.jpeg"
+
+export default function Home() {
+    return (
+        
+        <header className="entrance" style={{backgroundImage: `url(${background})`}}>
+            
+            <div className="banner">
+                <h1 className="banner-title">WELCOME</h1>
+                <h2>Here you can search any food recipe.</h2>
+            </div>
+        </header>
+    )
+}
