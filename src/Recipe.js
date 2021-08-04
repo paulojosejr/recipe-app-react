0b29b10e464a3a90a8d5f44478d4c0cbf1f97ed4
Paulo Araújo <paulojosejr95@gmail.com>
2021-08-04 09:20:36 +0100

    add new color and adjust ingredients align

--- src/Recipe.js
+++ src/Recipe.js
@@ -5,12 +5,12 @@ const Recipe = ({title, calories, image, ingredients}) => {
     return(
         <div className={style.recipe}>
             <h1 className={style.recipeTitle}>{title}</h1>
-            <ol>
+            <ul>
                 {ingredients.map((ingredient,index) => (
                     <li key={index} className={style.recipeIngredient}>{ingredient.text}</li>
                 ))}
-            </ol>
-                <p>Calories: {calories.toFixed(0)}</p>
+            </ul>
+                <p className={style.recipeCalories}>Calories: {calories.toFixed(0)}</p>
             <img className={style.image} src={image} alt=""/>
         </div>
     )
