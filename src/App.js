ab4b49e89569cdcdcbcfb0feffc021ca40e9fdef
Paulo Araújo <paulojosejr95@gmail.com>
2021-08-04 09:49:51 +0100

    add RecipeList components

--- src/App.js
+++ src/App.js
@@ -1,49 +1,9 @@
-import React, { useEffect, useState } from 'react'
-import Recipe from './Recipe'
-import './App.css';
+import React from 'react'
+import RecipeList from './components/RecipeList/RecipeList'
 
 const App = () => {
-
-  const [recipes, setRecipes] = useState([])
-  const [search, setSearch] = useState('')
-  const [query, setQuery] = useState('tapioca')
-
-  useEffect(() => {
-    getRecipes()
-  }, [query])
-
-  const getRecipes = async () => {
-    const response = await fetch(`../.netlify/functions/recipesFetch?query=${query}`)
-    const data = await response.json()
-    setRecipes(data.hits)
-  }
-
-  const updateSearch = e => {
-    setSearch(e.target.value)
-  }
-
-  const getSearch = e => {
-    e.preventDefault()
-    setQuery(search)
-    setSearch('')
-  }
   return (
-    <div className="App">
-      <form onSubmit={getSearch} className="search-form">
-        <input placeholder="Search your recipe" className="search-bar" type="text" value={search} onChange={updateSearch} />
-        <button className="search-button" type="submit">Search</button>
-      </form>
-      <div className="recipes">
-      {recipes.map(recipe => (
-        <Recipe 
-        key={recipe.recipe.calories}
-        title={recipe.recipe.label}
-        calories={recipe.recipe.calories}
-        image={recipe.recipe.image}
-        ingredients={recipe.recipe.ingredients}/>
-      ))}
-      </div>
-    </div>
+    <RecipeList />
   )
 }
 
---
+++ src/components/RecipeList/RecipeList.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react'
+import Recipe from '../Recipe/Recipe'
+import './RecipeList.css'
+
+const App = () => {
+
+    const [recipes, setRecipes] = useState([])
+    const [search, setSearch] = useState('')
+    const [query, setQuery] = useState('tapioca')
+  
+    useEffect(() => {
+      getRecipes()
+    }, [query])
+  
+    const getRecipes = async () => {
+      const response = await fetch(`../.netlify/functions/recipesFetch?query=${query}`)
+      const data = await response.json()
+      setRecipes(data.hits)
+    }
+  
+    const updateSearch = e => {
+      setSearch(e.target.value)
+    }
+  
+    const getSearch = e => {
+      e.preventDefault()
+      setQuery(search)
+      setSearch('')
+    }
+    return (
+      <div className="RecipeList">
+        <form onSubmit={getSearch} className="search-form">
+          <input placeholder="Search your recipe" className="search-bar" type="text" value={search} onChange={updateSearch} />
+          <button className="search-button" type="submit">Search</button>
+        </form>
+        <div className="recipes">
+        {recipes.map(recipe => (
+          <Recipe 
+          key={recipe.recipe.calories}
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+          ingredients={recipe.recipe.ingredients}/>
+        ))}
+        </div>
+      </div>
+    )
+  }
+  
+  export default App;
+  
